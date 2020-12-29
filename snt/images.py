23f2e6e225229f83381d878ea469c9045caf0563
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2020-12-29 22:22:45 +0100

    better print weight and height

--- snt/images.py
+++ snt/images.py
@@ -5,6 +5,8 @@ import PIL.Image as pil
 
 img = pil.open('data/paysage.jpeg')
 img.show()
+print(f'Largeur et hauteur de l"image: {img.size}')
+# print(img.size)
+# (600, 479)
 
-print(img.size)
 largeur, hauteur = img.size
