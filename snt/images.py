75fffadee3e11cf054e7ea6bf690d51463d24af7
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2020-12-31 19:20:20 +0100

    extraction canal blue et vert

--- snt/images.py
+++ snt/images.py
@@ -15,12 +15,19 @@ print(f'Largeur et hauteur de l"image: {img.size}')
 largeur, hauteur = img.size
 
 img_r = pil.new('RGB', (largeur, hauteur))
+img_g = pil.new('RGB', (largeur, hauteur))
+img_b = pil.new('RGB', (largeur, hauteur))
 
 for l in range(largeur):
     for h in range(hauteur):
         r,g, b = img.getpixel((l, h))
         img_r.putpixel((l, h), (r, 0, 0))
+        img_g.putpixel((l, h), (0, g, 0))
+        img_b.putpixel((l, h), (0, 0, b))
 
 img_r.show()
+img_g.show()
+img_b.show()
 img_r.save('paysage_rouge.png')
-
+img_g.save('paysage_vert.png')
+img_b.save('paysage_bleu.png')
