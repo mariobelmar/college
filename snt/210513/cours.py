9f35ea995c88c80713327dec080ea8d8982bb99e
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2021-05-15 15:31:02 +0200

    add cours.py

---
+++ snt/210513/cours.py
@@ -0,0 +1,8 @@
+print('Bonjour')
+age = 13
+
+s, l, d = 'hello', [22, 3.14, 'world'], {'age': 33, 'name': 'Luis'}
+#si j'appelle la variable s il repondra 'hello' 
+#'                      ' l il rpondra [22, 3.14, 'world']
+#etc
+
