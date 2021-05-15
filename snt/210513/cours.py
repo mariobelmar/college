812b5fbe3969db6314438e56d863cec48783aadd
Luis <luis@in-girum.net>
2021-05-15 23:28:09 +0200

    Papa add more to cours.py

--- snt/210513/cours.py
+++ snt/210513/cours.py
@@ -1,8 +1,32 @@
 print('Bonjour')
 age = 13
-
 s, l, d = 'hello', [22, 3.14, 'world'], {'age': 33, 'name': 'Luis'}
-#si j'appelle la variable s il repondra 'hello' 
-#'                      ' l il rpondra [22, 3.14, 'world']
-#etc
+nom = 'Luis' # input('votre nom: ')
+ss = f"""==
+    bonjour {nom}
+    comment allez vous ?
+    =="""
+
+print('=== String ===')
+print('== new lines with \"\"\" and \\n ==')
+
+s1 = 'xxx'
+s2 = 'xxx'
+s3 = 'xxx'
+s4 = 'xxx'
+
+print('== f-string ==')
+
+nom, age = 'Mario', 13
+
+ss = (f'l\'élève {nom}...\n'
+      f'   a {age} ans')
+print(ss)
+
+
+print('=== Control structures ===')
+
+for i in l:
+    if True:
+        print(i)
 
