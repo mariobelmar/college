dbea942c12ad6ec859470efdd3b69cab23730c3d
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2021-05-16 23:05:11 +0200

    exercies todo

--- snt/210513/cours.py
+++ snt/210513/cours.py
@@ -1,3 +1,4 @@
+# :w|!clear; python %
 print('Bonjour')
 age = 13
 s, l, d = 'hello', [22, 3.14, 'world'], {'age': 33, 'name': 'Luis'}
@@ -9,11 +10,12 @@ ss = f"""==
 
 print('=== String ===')
 print('== new lines with \"\"\" and \\n ==')
+print('Todo: create 3 multilines strings')
+
 
 s1 = 'xxx'
 s2 = 'xxx'
 s3 = 'xxx'
-s4 = 'xxx'
 
 print('== f-string ==')
 
@@ -25,8 +27,11 @@ print(ss)
 
 
 print('=== Control structures ===')
+print('Todo: put in a list ll all __builtins__ python methods, use '
+      's.islower and s.startswith')
 
+print(l)
+# [22, 3.14, 'world']
 for i in l:
     if True:
         print(i)
-
