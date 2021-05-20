dcc7303f1776609e4c05c7421065a11bfff378e0
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2021-05-20 21:29:40 +0200

    new request and beautify

--- snt/210513/cours.py
+++ snt/210513/cours.py
@@ -1,17 +1,17 @@
 # :w|!clear; python %
-print('Bonjour')
 age = 13
 s, l, d = 'hello', [22, 3.14, 'world'], {'age': 33, 'name': 'Luis'}
+
+print('\n=== input ===')
 nom = 'Luis' # input('votre nom: ')
 ss = f"""==
     bonjour {nom}
     comment allez vous ?
     =="""
 
-print('=== String ===')
-print('== new lines with \"\"\" and \\n ==')
+print('\n=== String ===')
+print('\n== new lines with \"\"\" and \\n ==')
 print('Todo: create 3 multilines strings')
-
 
 s1 = """bonjour je m'ap-
 pelle mario"""
@@ -25,24 +25,18 @@ s3 = ('bonjour je m\'ap-\n'
       'pelle mario')
 print(s3)
 
-
-
 if s1 == s2 == s3:
     print('Bravo !\n')
 else:
-    print('\n:(\n')
+    print('\n:(')
 
-print('== f-string ==')
-
-
+print('\n== f-string ==')
 nom, age = 'Mario', 13
-
 ss = (f'l\'élève {nom}...\n'
       f'   a {age} ans')
 print(ss)
 
-
-print('=== Control structures ===')
+print('\n=== Control structures ===')
 print('Todo: put in a list ll all __builtins__ python methods, use '
       's.islower and s.startswith')
 
@@ -53,6 +47,10 @@ for s in dir(__builtins__):
     if s.islower():
         if not s.startswith('__'):
             res.append(s)
-
 print(len(res))
-print(res)
+print(res[-8:])
+
+print('\n=== Comprehensive list')
+print('redo the above with a Comprehensive list')
+
+
