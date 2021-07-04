f2fa9ea632712a65344976700786bb88e3833b04
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2021-07-04 18:52:45 +0200

    api list

--- snt/api_list.py
+++ snt/api_list.py
@@ -1,11 +1,11 @@
-s.append
-s.clear
-s.copy
-s.count
-s.extend
-s.index
-s.insert
-s.pop
-s.remove
-s.reverse
-s.sort
+s.append  #[1 ,2 , 4].append('a') => [1,2,4, 'a']
+s.clear   #[1 ,2 , 4].clear()  => []
+s.copy    ?
+s.count   #['b', 'b', 'a'].count('a') => 1
+s.extend  #?
+s.index   #['b', 'b', 'a'].count('b') => 2
+s.insert  ?
+s.pop     #['b', 'b', 'a'].pop() => 'a'  ;  ['b', 'b']
+s.remove  #['b', 'b', 'a'].remove('b')  =>  ['b', 'a']
+s.reverse  #['c', 'b', 'a'].reverse()   => ['a' ,'b', 'c'
+s.sort     #[4, 3, 2, 1].sort() => [1, 2, 3, 4]
