04a7c48749549ebc05b22f2842bd3ad3a82dee93
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2021-09-05 19:14:56 +0200

    builtins dictionary and list done

--- snt/api_dict.py
+++ snt/api_dict.py
@@ -1,12 +1,10 @@
-d.clear    ?
+d.clear      #{'age': 3, '2':4}.clear =>  d = {}
+d.get        #{'age': 3, '2':4}.get('2')   =>  4
+d.pop        #{'age': 3, '2':4}.pop('age')  =>  3
+d.copy       #{'age': 3, '2':4}.copy()  =>  {'age': 3, '2': 4}
+d.items      #{'age': 3, '2':4}.items()  =>  [('age', 3), ('2', 4)]
+d.values     #{'age': 3}.values()  => ~ [3]
+d.keys       #{'age': 3}.keys() =>  ~ [age]
+d.setdefault #{'age': 3}.setdefault('a', 4)  =>  {'age': 3, 'a': 4}
+d.fromkeys   #d = {'age' : 3}.fromkeys('dc', 'ab')   =>  d= {'d': 'ab', 'c': 'ab'}
 
-d.get
-d.pop
-d.update
-d.copy
-d.items
-d.popitem
-d.values
-d.fromkeys
-d.keys
-d.setdefault
