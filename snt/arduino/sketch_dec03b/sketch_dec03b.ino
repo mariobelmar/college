94e974d16ab6d414dd2d145787b56a8cfe70d281
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-02-05 10:16:46 +0100

    add arduino code

---
+++ snt/arduino/sketch_dec03b/sketch_dec03b.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.bein(9600);
+  Serial.prinln("hello, cela vient de la fonction setup");
+ 
+ in maVariable = 27
+Serial.print("cecci est le contenu de ta variable");
+Serial.println(maVariable);
+delay(1000);n
