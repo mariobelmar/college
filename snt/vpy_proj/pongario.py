3722396b3400702f9503824691e4dc848d60b69f
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-02-25 23:11:31 +0100

    make it work

--- snt/vpy_proj/pongario.py
+++ snt/vpy_proj/pongario.py
@@ -8,36 +8,46 @@ from math import sin, cos
 
 ball = sphere(pos=vector(-5, 0, 0), radius=0.5, color=color.blue)
 
-wallr = box(pos=vector(6, 0, 0),
+wallrx = box(pos=vector(6, 0, 0),
             size=vector(0.2, 12, 12),
             color=color.red)
-walll = box(pos=vector(-6, 0, 0),
+walllx = box(pos=vector(-6, 0, 0),
             size=vector(0.2, 12, 12),
         color=color.red)
-wallb = box(pos=vector(0, 6, 0),
+wallby = box(pos=vector(0, 6, 0),
             size=vector(12, 0.2, 12),
             color=color.red)
-wallu = box(pos=vector(0, -6, 0),
+walluy = box(pos=vector(0, -6, 0),
             size=vector(12, 0.2, 12),
             color=color.red)
 
 
-ball.velocity = vector(25, 25, 0)
+ball.velocity = vector(5, 100, 0)
 
 deltat = 0.005
 t = 0
 ball.pos = ball.pos + ball.velocity * deltat
 i = 0
-while t < 0.5:
-    rate(70)
+while True:  # :t < 2.5:
+    rate(100)
     if i % 20 == 0:
         print(f'ball.pos: {ball.pos}, ball.velocity: {ball.velocity}')
-    if ball.pos.x > wallr.pos.x:
+    if ball.pos.x > wallrx.pos.x:
         ball.velocity.x = -ball.velocity.x
     ball.pos = ball.pos + ball.velocity * deltat
-    if ball.pos.x < -5 and ball.velocity.x < 0:
-        ball.velocity.x = 0
+
     t = t + deltat
     i = i + 1
+
+    if ball.pos.x < walllx.pos.x:
+        ball.velocity.x = -ball.velocity.x
+    ball.pos = ball.pos + ball.velocity * deltat
+
+    if ball.pos.y > walluy.pos.y:
+        ball.velocity.y = -ball.velocity.y
+    ball.pos = ball.pos + ball.velocity * deltat
+
+    if ball.pos.y < wallby.pos.y:
+        ball.velocity.y = -ball.velocity.y
 
 
