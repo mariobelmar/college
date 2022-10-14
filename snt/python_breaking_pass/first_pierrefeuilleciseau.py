3a0191bd14bbfe033fe399b51b70260512a6ecd8
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-10-15 00:24:56 +0200

    start pierre feuille ciseau

---
+++ snt/python_breaking_pass/first_pierrefeuilleciseau.py
@@ -0,0 +1,23 @@
+def winner(human, ordi):
+     if h == "sc":
+         if o == "pa":
+             return print("human winner")
+             elif o == "sc":
+                 return print("egality")
+                 else  o == "st":
+                         return print("computer winner")
+         elif h == "pa":
+             if o == "pa":
+                 return print("egality")
+                 elif o == "sc":
+                     return print("computer winner")
+                     else o == "st":
+                         return print("human winner")
+         else h == "st":
+             if o == "pa":
+                 return print("computer winner")
+                 elif o == "sc":
+                     return print("human winner")
+                     else o == "st":
+                         return print("egality")
+
