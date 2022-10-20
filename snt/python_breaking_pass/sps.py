e4e313d69f8c9e7862d743f5c6663d99fc004fd7
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-10-20 20:48:13 +0200

    add some folder

--- snt/python_breaking_pass/sps.py
+++ snt/python_breaking_pass/sps.py
@@ -1,10 +1,19 @@
 def winner(human, ordi):
     ow = "ordi win"
+    ega = "egality"
+    hw = "human win"
     if human == 'pa':
         if ordi == 'sc':
             return ow
+        if ordi =='pa':
+            return ega
+        if ordi == 'st':
+            return hw
+    if human   == 'sc':
+        if ordi == '' 
     return human, ordi
 
 if __name__ == '__main__':
     print(winner('pa', 'pa'))
     print(winner('pa', 'sc'))
+    print(winner('pa', 'st'))
