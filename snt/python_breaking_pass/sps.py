a655b218493f45c221d9616d0b5cbc907ec6ee6d
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-10-22 14:42:45 +0200

    firt pss working

--- snt/python_breaking_pass/sps.py
+++ snt/python_breaking_pass/sps.py
@@ -9,11 +9,29 @@ def winner(human, ordi):
             return ega
         if ordi == 'st':
             return hw
-    if human   == 'sc':
-        if ordi == '' 
+    if human == 'sc':
+        if ordi == 'sc':
+            return ega
+        if ordi == 'pa':
+            return ow
+        if ordi == 'st':
+            return hw
+    if human == 'st':
+        if ordi == 'sc':
+            return hw
+        if ordi == 'pa':
+            return ow
+        if ordi == 'st':
+            return ega
     return human, ordi
 
 if __name__ == '__main__':
     print(winner('pa', 'pa'))
     print(winner('pa', 'sc'))
     print(winner('pa', 'st'))
+    print(winner('sc', 'pa'))
+    print(winner('sc', 'sc'))
+    print(winner('sc', 'st'))
+    print(winner('st', 'pa'))
+    print(winner('st', 'sc'))
+    print(winner('st', 'st'))
