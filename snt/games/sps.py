03b87fd22ca452961862b2fbac3a15d5476a6b5e
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-10-28 19:59:37 +0200

    who win ?

--- snt/games/sps.py
+++ snt/games/sps.py
@@ -7,19 +7,31 @@ def winner(human, ordi) -> str:
     >>> winner('pa', 'sc')
     'sc'
     >>>
+    >>> winner('pa', 'sc')
+    'sc', 'Human'
+    >>>
     """
+    # return 'sc', 'Human'
     egal = 'egality'
-    for v_ordi, v_human, winner in GAMES_RULES:
+    for v_ordi, v_human, w_choice in GAMES_RULES:
         if human == ordi:
-            return egal
+            return egal, None
         if human ==  v_human and ordi == v_ordi:
-            return winner
+            w_name = 'human' if w_choice == human else 'ordi'
+            return w_choice, w_name
         if human ==  v_ordi and ordi == v_human:
-            return winner
+            w_name = 'human' if w_choice == human else 'ordi'
+            return w_choice, w_name
 
 
 def show(human, ordi):
-    return f"{winner(human, ordi):15}: human a joué {human}, ordi joué {ordi}"
+    w_choice, w_name = winner(human, ordi)
+    if human == ordi :
+        return 'exequo'
+    return f"Le gagnant est {w_name} il a jouéé {w_choice}, ordi joué {ordi}"
+
+
+
 
 
 if __name__ == '__main__':
