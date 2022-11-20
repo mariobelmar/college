7111775bff2fa25e18364b935f0d5a34f429b254
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-11-20 17:04:05 +0100

    start nuage de mots project

---
+++ snt/ndm/ndm.py
@@ -0,0 +1,33 @@
+import spacy
+import sys
+
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("en_core_web_sm")
+
+
+with open("alice.txt", 'r') as myfile:
+    # Process whole documents
+    text = myfile.read()
+    mots = text.split()
+
+text = text[2000:2400]
+
+
+doc = nlp(text)
+
+
+# Analyze syntax
+# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+
+verbe = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+
+
+# Find named entities, phrases and concepts
+# for entity in doc.ents:
+#     print(entity.text, entity.label_)
+
+print(f'Dans Alice opdm il y a:\n'
+      f' {len(mots)} mots\n'
+      f'et dans un échantillon de 400 mots\n il y a {len(verbe)} verbe'
+      f'en voici la liste:  {verbe}')
