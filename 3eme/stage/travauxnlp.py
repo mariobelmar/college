868f65f4909d489dd857397cbf910e54089353b6
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-12-11 19:30:31 +0100

    add functions

--- 3eme/stage/travauxnlp.py
+++ 3eme/stage/travauxnlp.py
@@ -6,21 +6,21 @@ nlp = spacy.load("en_core_web_sm")
 NOVEL = '../../snt/ndm/alice.txt'
 
 def get_text_from_file(file: str) -> str:
-    """""
+    """
     ouvrir le fichier texte 'alice.txt' ou permettre a python de le lire 'r'
     myfile représente le fichier texte il peut donc en faire plusieurs chose
-    """""
+    """
     with open(file, 'r') as myfile:
         text = myfile.read()
     return text
 
 
 def get_only_verbs(ltext: str) -> list[str]:
-    """""
+    """
     for 'mot' in doc:
         if 'nature du mot' == verbe:
             verbe.append('infinitif du verbe')
-    """""
+    """
     doc = nlp(ltext)
     # verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
     verbs = []
