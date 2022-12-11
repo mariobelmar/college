0fcf85ef450bc83dbbe6aed216907682461cbda6
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-12-11 19:26:24 +0100

    improve code

--- 3eme/stage/travauxnlp.py
+++ 3eme/stage/travauxnlp.py
@@ -5,17 +5,22 @@ import sys
 nlp = spacy.load("en_core_web_sm")
 NOVEL = '../../snt/ndm/alice.txt'
 
-# ouvrir le fichier texte 'alice.txt' ou permettre a python de le lire 'r'
-# myfile représente le fichier texte il peut donc en faire plusieurs chose
-with open(NOVEL, 'r') as myfile:
-    text = myfile.read()
-    mots = text.split()
-
-# sample
-ltext = text[2000:3000]
+def get_text_from_file(file: str) -> str:
+    """""
+    ouvrir le fichier texte 'alice.txt' ou permettre a python de le lire 'r'
+    myfile représente le fichier texte il peut donc en faire plusieurs chose
+    """""
+    with open(file, 'r') as myfile:
+        text = myfile.read()
+    return text
 
 
 def get_only_verbs(ltext: str) -> list[str]:
+    """""
+    for 'mot' in doc:
+        if 'nature du mot' == verbe:
+            verbe.append('infinitif du verbe')
+    """""
     doc = nlp(ltext)
     # verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
     verbs = []
@@ -24,10 +29,10 @@ def get_only_verbs(ltext: str) -> list[str]:
             verbs.append(token.lemma_)
     return verbs
 
-# for 'mot' in doc:
-#     if 'nature du mot' == verbe:
-#         verbe.append('infinitif du verbe')
 
+text = get_text_from_file(NOVEL)
+ltext = text[2000:4500]
 verbs = get_only_verbs(ltext)
+print(f'==> all words: {ltext}')
 print(f'==> only verbs: {verbs}')
 
