b5b4e5002779af9e8f85e6efe019fcd3480c7a4c
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-12-16 18:27:20 +0100

    finish work in test.py start to put in travauxnlp.py

--- 3eme/stage/test.py
+++ 3eme/stage/test.py
@@ -5,7 +5,7 @@ import pandas as pd
 nlp = spacy.load("en_core_web_sm")
 
 FILES = ["aari_1994_o.txt"]
-         # "abun_1995_o.txt"]
+         # "abun_1995_o.txt",
          # "abun_19952_o.txt",
          # "abu_1985_o.txt",
          # "abun_19952_o.txt",
@@ -31,32 +31,33 @@ def main(FILES):
         text = get_text_from_file(filename)
         doc = nlp(text)
         liste1 = [token.lemma_ for token in doc if token.pos_ not in ['PUNCT', 'SPACE']]
-        for i in set(liste1):
-            occu, mot = liste1.count(i), i
+        for t in set(liste1):
+            occu, mot = liste1.count(t), t
             line = {
+                'fichier': i,
                 'mots': mot,
                 'occurence': occu,
+                'ratio': occu / len(text)
             }
             lines.append(line)
     return pd.DataFrame(lines)
 
 
+def mediane(liste1):
+    a = len(liste1)
+    b = int(a/2)
+    c = b + 1
+    if b // 2 == 1:
+        b = sum(liste1[b], liste1[c])/2
+    return liste1[b]
+
+
 if __name__ == '__main__':
-    print(main(FILES))
-    # for i in FILES:
-    #     filename = f'grammartext/{i}'
-    #     text = get_text_from_file(filename)
-    #     doc = nlp(text)
-    #     mot = text.split()
-    #     mots = [token for token in doc] 
-    #     print(mots[500:750])
-    #     # for i in text:
-    #     #     print(i)
-
-
-
-
-
+    df = main(FILES)
+    df.to_excel('test.xlsx')
+    df2 = (df.sort_values(by=("occurence"), ascending=False)) 
+    occu = list(df.occurence)
+    print(mediane(occu))
 
 
 
