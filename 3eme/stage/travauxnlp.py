355132990ad9ae736fb3db19f87dfd6de89b28c9
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-12-19 22:51:04 +0100

    totally finish

--- 3eme/stage/travauxnlp.py
+++ 3eme/stage/travauxnlp.py
@@ -1,15 +1,22 @@
 """
-$ python travauxnlp.py
-in aari_1990_o.txt      genders :   6 |tones:   4 | times:   4| pour_gender: 0.004% | pour_tone:0.003% | pour_time: 0.003%
-in aari_1994_o.txt      genders :   6 |tones:   0 | times:   2| pour_gender:     0% | pour_tone:    0% | pour_time:     0%
-in abu_1985_o.txt       genders :  27 |tones:   7 | times:  63| pour_gender: 0.003% | pour_tone:0.001% | pour_time: 0.008%
-in abun_19952_o.txt     genders :   1 |tones:  39 | times:  54| pour_gender:   0.0% | pour_tone: 0.01% | pour_time: 0.014%
-in abun_1995_o.txt      genders :   0 |tones:   1 | times:  89| pour_gender:     0% | pour_tone:    0% | pour_time:     0%
-in abun_1999_o.txt      genders :   2 |tones:  44 | times: 145| pour_gender:   0.0% | pour_tone:0.006% | pour_time: 0.021%
-in ani_2000_o.txt       genders :  14 |tones:   6 | times:   4| pour_gender: 0.028% | pour_tone:0.012% | pour_time: 0.008%
-$
+              langue gender/tone  k.occu  median_occurence  difference   has?
+0     ani_2000_o.txt     genders      16                 1          15   True
+1     ani_2000_o.txt       tones       7                 1           6   True
+2    aari_1994_o.txt     genders       6                 1           5   True
+3    aari_1994_o.txt       tones       0                 1          -1  False
+4   abun_19952_o.txt     genders       1                 1           0  False
+5   abun_19952_o.txt       tones      44                 1          43   True
+6     abu_1985_o.txt     genders      30                27           3   True
+7     abu_1985_o.txt       tones       8                27         -19  False
+8   abun_19952_o.txt     genders       1                 1           0  False
+9   abun_19952_o.txt       tones      44                 1          43   True
+10   abun_1999_o.txt     genders       2                 1           1   True
+11   abun_1999_o.txt       tones      49                 1          48   True
+12   aari_1990_o.txt     genders       6                 1           5   True
+13   aari_1990_o.txt       tones       4                 1           3   True
+14   abun_1995_o.txt     genders       0                 1          -1  False
+15   abun_1995_o.txt       tones       2                 1           1   True
 """
-
 import spacy
 # import sys
 # import csv
@@ -19,14 +26,14 @@ import pandas as pd
 # Définir la langue (an)
 nlp = spacy.load("en_core_web_sm")
 
-FILES = ["ani_2000_o.txt"]
-         # "aari_1994_o.txt",
-         # "abun_19952_o.txt",
-         # "abu_1985_o.txt",
-         # "abun_19952_o.txt",
-         # "abun_1999_o.txt",
-         # "aari_1990_o.txt",
-         # "abun_1995_o.txt"]
+FILES = ["ani_2000_o.txt",
+         "aari_1994_o.txt",
+         "abun_19952_o.txt",
+         "abu_1985_o.txt",
+         "abun_19952_o.txt",
+         "abun_1999_o.txt",
+         "aari_1990_o.txt",
+         "abun_1995_o.txt"]
 
 
 def get_text_from_file(file: str) -> str:
@@ -59,59 +66,41 @@ def mediane(liste1):
     return liste1[b]
 
 
-def exist_or_not_tones(tones, mediane: int) -> str:
-    bad = "this system doesn't exist in this language"
-    good = 'this system exist in this language'
-    if mediane < tones:
-        return good
-    return bad
-
-
-def exist_or_not_genders(genders, mediane: int) -> str:
-    bad = "there isn't genders in this language"
-    good = 'there is tones in this language'
-    if mediane < genders:
-        return good
-    return bad
-
-
-def put_all_word_in_table(FILES):
-    """
-    nom du fichier | mot | occurence
-
-    """
-    lines = []
-    for i in FILES:
-        filename = f'grammartext/{i}'
-        text = get_text_from_file(filename)
-        doc = nlp(text)
-        liste1 = [token.lemma_ for token in doc if token.pos_ not in ['PUNCT', 'SPACE']]
-        for t in set(liste1):
-            occu, mot = liste1.count(t), t
-            line = {
-                'langue': i,
-                'word': mot,
-                'word_occurence': occu,
-                   }
-            lines.append(line)
-    return pd.DataFrame(lines)
-
-
-
-
 def put_test_in_table (files):
     lines = []
     for i in files:
         filename = f'grammartext/{i}'
         text = get_text_from_file(filename)
+        doc = nlp(text)
         genders, tones = test_all_files(text)
-        line1 = {'langue': i,
+        liste1 = [token.lemma_ for token in doc if token.pos_ not in ['PUNCT', 'SPACE']]
+        pre_median = []
+        for t in set(liste1):
+            occu = liste1.count(t)
+            pre_median.append(occu)
+        median = mediane(pre_median)
+        gender_diff = len(genders) - median
+        tone_diff  = len(tones) - median
+        if gender_diff > 0:
+            msg1 = True
+        else:
+            msg1 = False
+        if tone_diff > 0:
+            msg2 = True
+        else:
+            msg2 = False
+        line1 = {'langue': i[:-4],
                  'gender/tone': 'genders',
-                 'k.occu': len(genders)}
-
-        line2 = {'langue': i,
+                 'k.occu': len(genders),
+                 'median_occurence': median,
+                 'difference': gender_diff,
+                 'has?': msg1}
+        line2 = {'langue': i[:-4],
                  'gender/tone': 'tones',
-                 'k.occu': len(tones)}
+                 'k.occu': len(tones),
+                 'median_occurence': median,
+                 'difference': tone_diff,
+                 'has?': msg2}
         lines.append(line1)
         lines.append(line2)
     return pd.DataFrame(lines)
@@ -119,13 +108,4 @@ def put_test_in_table (files):
 
 if __name__ == '__main__':
     test = put_test_in_table(FILES)
-    all_word2 = put_all_word_in_table(FILES)
-    # all_word2 = (all_word.sort_values(by=("occurence"), ascending=False))
-    occu = list(all_word2.word_occurence)
     print(test)
-    print('\n')
-    print(all_word2)
-    median = mediane(occu)
-    nb_gender = (int(test.genders))
-    answer_gender = exist_or_not_genders(nb_gender, median)
-    print(answer_gender)
