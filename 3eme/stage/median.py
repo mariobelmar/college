771e99a8302f59abd3aa3b641d1eb5df60c3ce45
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-12-29 11:21:07 +0100

    odd case docstring

--- 3eme/stage/median.py
+++ 3eme/stage/median.py
@@ -10,7 +10,12 @@ def medianne(ll: list[int]) -> int:
                           L  R
                           1  2
       median = (L + R)/2
-    else:  # e.g. l = [2, 6, 6 , 30, 5]
+    else:
+      # e.g. l = [2, 6, 6 , 30, 5]
+                        ^
+                        C idx:2
+      len(l)/2 == 2.5
+      floor(2.x) == 2
       median = l[floor(len(l)/2)]
     """
     liste = ll.copy()
@@ -19,25 +24,27 @@ def medianne(ll: list[int]) -> int:
     if size % 2 == 0:
         R_idx = int((size / 2))
         L_idx = int((size / 2) - 1)
-        R = liste[L_idx]
-        L = liste[R_idx]
-        median = (L + R) / 2
+        median = (liste[L_idx] + liste[R_idx]) / 2
     else:
-        mid = 0  # floor(size / 2)
+        pass
+        # mid = floor(size / 2)
+        # print('mid')
+
     # mid = int(size/2)
     # mid1 = mid + 1
     # # if even
     # if not size % 2 == 0:
     #     mid = int((liste[mid] + liste[mid1]) / 2)
     # # if odd
+
     return median
 
 
 if __name__ == '__main__':
 
     liste = [2, 4 ,3, 6, 6 ,3 ,7, 10, 30, 2]
-    liste = [2, 6, 6 , 30, 5]
     liste = [1, 7, 6 , 0]
+    liste = [2, 6, 6 , 30, 5]
 
     print(liste)
     print(f'mine :{medianne(liste)}')
