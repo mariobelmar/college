c10416f0880b5b63a3acddace0ccd2d27f505433
Mario Belmar Letelier <mariobelmarmath@gmail.com>
2022-12-29 17:58:19 +0100

    merge median and travauxnlp

--- 3eme/stage/travauxnlp.py
+++ 3eme/stage/travauxnlp.py
@@ -26,14 +26,34 @@ def test_all_files(text):
     genders = [token for token in doc if token.text in ['gender', 'genders']]
     return genders, tones
 
-def medianne(liste1):
-    liste2 = sorted(liste1)
-    nb = len(liste1)
-    mid = int(nb/2)
-    mid1 = mid + 1
-    if not nb % 2 == 0:
-        mid = int((liste2[mid] + liste2[mid1]) / 2)
-    return liste2[mid]
+
+def medianne(ll: list[int]) -> int:
+    """
+    if even: e.g. l = [1, 7, 6, 0]
+                          ^  ^
+                          L  R
+                          1  2
+      median = (L + R)/2
+    else:
+      # e.g. l = [2, 6, 6 , 30, 5]
+                        ^
+                        C idx:2
+      len(l)/2 == 2.5
+      floor(2.x) == 2
+      median = l[floor(len(l)/2)]
+    """
+    liste = ll.copy()
+    liste.sort()
+    size = len(liste)
+    if size % 2 == 0:
+        R_idx = int((size / 2))
+        L_idx = int((size / 2) - 1)
+        median = (liste[L_idx] + liste[R_idx]) / 2
+    else:
+        mid = int(floor(size/2))
+        median = liste[mid]
+
+    return median
 
 
 def put_test_in_table (files):
